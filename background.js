733efc52f99d4f715f7c35738ae37642aa6fad3d
Alex Snow <asnowman7@gmail.com>
2018-02-07 14:43:20 -0500

    updated backend to use custom eval()

--- background.js
+++ background.js
@@ -22,7 +22,8 @@ function numberButton(number) {
  * @param {string} operator - Arithmetic operation to be added  
  */
 function operatorButton(operator) {
-    document.getElementById('screen').innerHTML = eval(buffer);
+    // add a check to make sure buffer does not contain consecutive operators
+    document.getElementById('screen').innerHTML = evaluate(buffer);
     buffer += ' ' + operator + ' ';
     currentNumber = ""
 }
@@ -32,7 +33,7 @@ function operatorButton(operator) {
  * sets it to screen, and clears currentNumber.
  */
 function equalButton() {
-    var newVal = eval(buffer);
+    var newVal = evaluate(buffer);
     buffer = newVal.toString();
     document.getElementById('screen').innerHTML = newVal;
     currentNumber = "";
@@ -67,4 +68,37 @@ function backSpaceButton() {
     } else {
         document.getElementById('screen').innerHTML = currentNumber;
     }
+}
+
+/**
+ * Splits buffer into array of integers and operators, and 
+ * evaluates them; to replace eval().
+ */
+function evaluate(theBuffer) {
+    var result = 0,
+        bufferArray,
+        operators = ["+", "-", "*", "/"],
+        i,
+        currentOp;
+    bufferArray = theBuffer.split(" ");
+    for (i = 0, len = bufferArray.length; i < len; i++) {
+        // have we previously seen an arithmetic operator
+        if (typeof currentOp == "string") {
+            if (currentOp == "+") result = result + parseInt(bufferArray[i]);
+            if (currentOp == "-") result = result - parseInt(bufferArray[i]);
+            if (currentOp == "*") result = result * parseInt(bufferArray[i]);
+            if (currentOp == "/") result = result / parseInt(bufferArray[i]);
+            currentOp = 0;
+        // we have not previously seen an arithmetic operator
+        } else {
+            // if current character is an arithmetic operator
+            if (operators.indexOf(bufferArray[i]) > -1) {
+                currentOp = bufferArray[i];
+            // else current character is an integer
+            } else {
+                result += parseInt(bufferArray[i]);
+            }
+        }
+    }
+    return result;
 }
